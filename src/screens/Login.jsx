4f673ede44cc9c629d74f63756a8129cbf1a3c0b
BuiHai1502 <122037456+haibdhe141497@users.noreply.github.com>
2023-01-30 22:15:26 +0700

    Update Register

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -8,22 +8,29 @@ import AuthenNavbar from "../components/Nav/AuthenNavbar"
 
 
 export default function Login(props) {
-  const [email, setEmail] = useState('');
-  const [pass, setPass] = useState('');
+  // const [email, setEmail] = useState('');
+  // const [pass, setPass] = useState('');
   const [message, setMessage] = useState("");
 
-  //Declaring the rule of characters to be used. Applying rule to current state. Verifying the result.
-  const emailRegex = /[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,8}(.[a-z{2,8}])?/g;
-  //    length 6 to 16.
-  //    One Alphabet in Password.
-  //    One Special Character in Password.
-  const passwordRegEx = "^(?=.*[a-z])(?=.*[$@$#!%*?&])[A-Za-z\\d$@$#!%*?&]{6,16}";
+  const [values, setValues] = useState({
+    email:'',
+    password:''
+  });
 
-  const emailValidation = () => {
-    const regEx = /[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,8}(.[a-z{2,8}])?/g;
-    if (regEx.test(email)) {
+
+  // //Declaring the rule of characters to be used. Applying rule to current state. Verifying the result.
+  // const emailRegex = /[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,8}(.[a-z{2,8}])?/g;
+  // //    length 6 to 16.
+  // //    One Alphabet in Password.
+  // //    One Special Character in Password.
+  // const passwordRegEx = "^(?=.*[a-z])(?=.*[$@$#!%*?&])[A-Za-z\\d$@$#!%*?&]{6,16}";
+
+  const Validation = () => {
+    const emailregEx = /[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,8}(.[a-z{2,8}])?/g;
+    const passwordRegEx = "^(?=.*[a-z])(?=.*[$@$#!%*?&])[A-Za-z\\d$@$#!%*?&]{6,16}";
+    if (emailregEx.test(values.email)&&passwordRegEx.test(values.password)) {
       setMessage("Email is Valid");
-    } else if (!regEx.test(email) && email !== "") {
+    } else if ((!emailregEx.test(values.email) && values.email !== "")||(!passwordRegEx.test(values.password) && values.password !== "")) {
       setMessage("Email is Not Valid");
     } else {
       setMessage("");
@@ -31,15 +38,16 @@ export default function Login(props) {
   };
 
   const handleOnChange = (e) => {
-    setEmail(e.target.value);
+    setValues({...values,[e.target.name]:e.target.value});
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    Validation();
 
     axios.post('http://localhost:8080/login', {
-      'username' : email,
-      'password' : pass
+      'username' : values.email,
+      'password' : values.pass
     })
     .then(function (response) {
       console.log(response);
@@ -57,15 +65,15 @@ export default function Login(props) {
           <h2>Login</h2>
           <form className="login-form" onSubmit={handleSubmit}>
             <label className="semiBold" htmlFor="email">Email</label>
-            <input id="email" className="input" type="email" placeholder="email" value={email} onChange={handleOnChange} />
+            <input id="email" name="email" className="input" type="email" placeholder="email" value={values.email} onChange={handleOnChange} />
 
             <label className="semiBold " htmlFor="password">Password</label>
-            <input className="marginBot" value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+            <input id="password" name="password" className="marginBot"  type="password" value={values.password} onChange={handleOnChange}  placeholder="********"  />
             <FullButton title="Get Started" />
 
           </form>
           <RouterLink to="/register">
-            <button className="link-btn " onClick={(emailValidation) => props.onFormSwitch('register')}>Don't have an account?  <span className="semiBold"> Register here.</span></button>
+            <button className="link-btn " onClick={() => props.onFormSwitch('register')}>Don't have an account?  <span className="semiBold"> Register here.</span></button>
                 
               </RouterLink>
           {/* <button className="link-btn " onClick={(emailValidation) => props.onFormSwitch('register')}>Don't have an account?  <span className="semiBold"> Register here.</span></button> */}
